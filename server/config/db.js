028620327850efe0c8a8119482dacde05d03d1e3
Jeet <jeet69554@gmail.com>
2026-09-07 21:46:34 +0530

    Connect frontend to live backend

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected:', process.env.MONGO_URI);
+    console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
     process.exit(1);
